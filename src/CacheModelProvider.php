107f70b74ee285814e6d133994603d5db0d48d19
titrxw <1120309488@qq.com>
2019-11-28 11:48:51 +0800

    修改事物listener异常问题

--- src/CacheModelProvider.php
+++ src/CacheModelProvider.php
@@ -63,7 +63,7 @@ class CacheModelProvider extends ProviderAbstract {
 	private function registerListener() {
 		//处理在事物中使用缓存问题
 		Model::getEventDispatcher()->listen(TransactionCommitted::class, function (TransactionCommitted $instance) {
-			if ($instance->connection->transactionLevel() !== 0) {
+			if ($instance->connection->transactionLevel() !== 0 || empty($instance->connection->transCallback)) {
 				return false;
 			}
 			$callbacks = $instance->connection->transCallback;
