5b238a9c753125099ca4533a9364f9bb8e5bb73c
titrxw <1120309488@qq.com>
2020-04-16 16:04:15 +0800

    升级model cache版本

--- src/Command/ClearCommand.php
+++ src/Command/ClearCommand.php
@@ -27,17 +27,15 @@ class ClearCommand extends CommandAbstract {
 	protected function handle($options) {
 		$option = $options['model'];
 
-		go(function () use ($option) {
-			if (!$option) {
-				if ($this->output->confirm('clear all model cache?')) {
-					return $this->flushEntireCache();
-				}
-				$this->output->error("option model Can't be empty");
-				return false;
+		if (!$option) {
+			if ($this->output->confirm('clear all model cache?')) {
+				return $this->flushEntireCache();
 			}
+			$this->output->error("option model Can't be empty");
+			return false;
+		}
 
-			return $this->flushModelCache($option);
-		});
+		return $this->flushModelCache($option);
 	}
 
 	protected function flushEntireCache() : int {
